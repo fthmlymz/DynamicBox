41d4689836ce144d8b5896e93be7525b121716a0
fthmlymz <fthmlymz@gmail.com>
2022-02-09 16:39:49 +0300

    updated products

--- Frontend/PurchasingManagement/src/app/models/material-demand-detail/material-demand-detail.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MaterialDemandDetail } from './material-demand-detail';
-
-describe('MaterialDemandDetail', () => {
-  it('should create an instance', () => {
-    expect(new MaterialDemandDetail()).toBeTruthy();
-  });
-});
--- Frontend/PurchasingManagement/src/app/models/material-demand-detail/material-demand-detail.ts
+++
@@ -1,9 +0,0 @@
-export class MaterialDemandDetail {
-  stockNo:string | undefined;
-  stockName:string | undefined;
-  total:string | undefined;
-  price:number | undefined;
-  createdDate: Date | undefined;
-  updatedDate: Date | undefined;
-  businessCode: string | undefined;
-}
